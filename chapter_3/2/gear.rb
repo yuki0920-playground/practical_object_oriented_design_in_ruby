2cd86776e3cee5e8bff252941923ce6449d920b4
yuki0920 <ywsep20@gmail.com>
2019-11-09 16:56:14 +0900

    initializeメソッドを固定されたパラメーターの代わりにオプションのハッシュを受け取れるようにした - 固定された順番の引数への依存を解消するため

--- chapter_3/2/gear.rb
+++ chapter_3/2/gear.rb
@@ -1,9 +1,9 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
-  def initialize(chainring, cog, wheel)
-    @chainring = chainring
-    @cog = cog
-    @wheel = wheel
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @wheel = args[:wheel]
   end
 
   def gear_inches
@@ -32,6 +32,6 @@ class Wheel
 end
 
 Gear.new(
-  52,
-  11,
-  Wheel.new(26, 1.5)).gear_inches
+  :chainring => 52,
+  :cog => 11,
+  :wheel => Wheel.new(26, 1.5)).gear_inches
