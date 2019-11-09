06b5667e396d3b512c2e784a24c4b9b783f1e138
yuki0920 <ywsep20@gmail.com>
2019-11-09 11:17:41 +0900

    Gearクラスからタイヤの直径を計算する責任を取り除いた - Wheel Structをブロックで拡張

--- gear.rb
+++ gear.rb
@@ -3,8 +3,7 @@ class Gear
   def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @tire = tire
-    @rim = rim
+    @wheel = Wheel.new(rim ,tire)
   end
 
   def ratio
@@ -12,11 +11,12 @@ class Gear
   end
 
   def gear_inches
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
+  end
+
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
-
-puts Gear.new(52, 11, 26, 1.5).gear_inches
-#-> 137.0909090909091
-puts Gear.new(52, 11, 24, 1.25).gear_inches
-#-> 125.27272727272728
