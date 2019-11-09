0d1929b050de62bfa9ce2372d81b1684618235ab
yuki0920 <ywsep20@gmail.com>
2019-11-09 16:14:15 +0900

    Gear初期化の際にdiarmeterに応答できるwheelオブジェクトを要求した

--- chapter_3/gear.rb
+++ chapter_3/gear.rb
@@ -1,14 +1,13 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
   end
 
   def gear_inches
-    ratio * Wheel.new(rim, tire).diameter
+    ratio * wheel.diameter
   end
 
   def ratio
