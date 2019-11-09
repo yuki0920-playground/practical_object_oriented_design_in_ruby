cded52b311bd782bb391fb03bd0b2a59c797dccf
yuki0920 <ywsep20@gmail.com>
2019-11-09 17:01:22 +0900

    fetchメソッドを使いデフォルト値の指定した - 引数のキーがargsハッシュにない時に、nilが返らないようになる(= デフォルト値が設定される)

--- chapter_3/2/gear.rb
+++ chapter_3/2/gear.rb
@@ -1,8 +1,8 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
   def initialize(args)
-    @chainring = args[:chainring] || 40
-    @cog = args[:cog] || 18
+    @chainring = args.fetche(:chainring, 40)
+    @cog = args.fetch(:cog, 18)
     @wheel = args[:wheel]
   end
 
