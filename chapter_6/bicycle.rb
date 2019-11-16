2359e4bbcc542e51f94dd5476c7ca24557a808ae
yuki0920 <ywsep20@gmail.com>
2019-11-16 15:13:18 +0900

    共通する属性をスーパークラスに昇格させる

--- chapter_6/bicycle.rb
+++ chapter_6/bicycle.rb
@@ -1,8 +1,10 @@
 class Bicycle
-  attr_accessor :size
+  attr_accessor :size, :chain, :tire_size
 
   def initialize(args={})
     @size = args[:size]
+    @chain = args[:chain]
+    @tire_size = args[:tire_size]
   end
 end
 
