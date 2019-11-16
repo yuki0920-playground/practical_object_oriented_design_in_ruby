73e235d78506c969dbd19a1883b479bb05b73855
yuki0920 <ywsep20@gmail.com>
2019-11-16 09:11:40 +0900

    クラスへの依存をなくした

--- chapter_5/2/trip.rb
+++ chapter_5/2/trip.rb
@@ -3,12 +3,11 @@ class Trip
 
   def prepare(preparers)
     preparers.each { |preparer| }
-    case preparer
-    when Mecanic
+    if preparer.responds_to?(:preapre_bicycles)
       preparer.prepare_bicucles(bicycyles)
-    when TripCordinator
+    elsif preparer.responds_to?(:buy_food)
       preparer.buy_food(customers)
-    when Driver
+    else preparer.responds_to?(:gas_up)
       preparer.gas_up(vehicle)
       preparer.fill_water_tank(vehicle)
     end
