de088a1a8870170489e681d9f96cb065e0a394a9
yuki0920 <ywsep20@gmail.com>
2019-11-16 10:38:41 +0900

    バイクにstyle属性を追加した

--- chapter_6/bicycle.rb
+++ chapter_6/bicycle.rb
@@ -1,26 +1,32 @@
 class Bicycle
-  attr_accessor :size, :tape_color
+  attr_accessor :style, :size, :tape_color, :front_shock, :rear_shock
 
   def initialize(args)
+    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
   # 全ての自転車は、デフォルト値として同じタイヤサイズとチェーンサイズを持つ
   def spares
-    {chain: '10-speed', tire_size: '23', tape_color: tape_color}
+    if style == :road
+      {chain: '10-speed', tire_size: '23', tape_color: tape_color}
+    else
+      {chain: '10-speed', tire_size: '2.1', rear_shock: rear_shock}
+    end
   end
 
   # 他にもメソッドたくさん
 end
 
 bike = Bicycle.new(
-  size: 'M',
-  tape_color: 'red'
+  style: :mountain,
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox',
 )
 
-bike.size #-> 'M'
 bike.spares
-# -> {:tire_size => "23",
-#     :chain => "10-speed",
-#     :tape_color => "red"}
+# -> {:chain=>"10-speed", :tire_size=>"2.1", :rear_shock=>"Fox"}
