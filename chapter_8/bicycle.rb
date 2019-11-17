c37e30694a90d46233e0001de754d01cdbbfae73
yuki0920 <ywsep20@gmail.com>
2019-11-17 09:38:23 +0900

    first commit(chapter_6の最後と同じ)

---
+++ chapter_8/bicycle.rb
@@ -0,0 +1,65 @@
+class Bicycle
+  attr_accessor :size, :chain, :tire_size
+
+  def initialize(args={})
+    @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args) # Bicycleでは送信と
+  end
+
+  def post_initialize(args) # 実装の両方を行う
+    nil
+  end
+
+  def default_chain
+    '10-speed'
+  end
+
+  def default_tire_size
+    raise NotImplementedError,
+      "This #{self.class} cannot respond to."
+  end
+
+  def spares
+    {tire_size: tire_size, chain: chain}.merge(local_spares)
+  end
+
+  # サブクラスがオーバーライドするためのフック
+  def local_spares
+    {}
+  end
+end
+
+class RoadBike < Bicycle
+  attr_accessor :tape_color
+
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+  end
+
+  def local_spares
+    {tape_color: tape_color}
+  end
+
+  def default_tire_size
+    '23'
+  end
+end
+
+class MouintainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def post_initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+  end
+
+  def local_spares
+    {rear_schock: rear_shock}
+  end
+
+  def default_tire_size
+    '2.1'
+  end
+end
